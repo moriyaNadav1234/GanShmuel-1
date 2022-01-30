88ab7c4f1db6a174ad4d6a602da5abd8331c145d
Long Vu <long.vu@sunbytes.vn>
2022-01-31 00:56:57 +0700

    add basic docker compose

--- devOps/webhook/process.sh
+++ devOps/webhook/process.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo test $1 >> test.log
+
+ref=$1
+branch_name=${ref##refs/heads/}
+
+git clone git@github.com:develeapDorZ/GanShmuel.git
+cd GanShmuel
+git checkout "${branch_name}"
+git fetch --prune
+git pull
+
+docker compose up -d
