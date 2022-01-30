402e5796507a87792afa39391c25a08237017c1d
Long Vu <long.vu@sunbytes.vn>
2022-01-31 01:15:03 +0700

    coding for process

--- devOps/webhook/process.sh
+++ devOps/webhook/process.sh
@@ -5,10 +5,14 @@ echo test $1 >> test.log
 ref=$1
 branch_name=${ref##refs/heads/}
 
+cd /home/ec2-user/app/
+
 git clone git@github.com:develeapDorZ/GanShmuel.git
 cd GanShmuel
 git checkout "${branch_name}"
 git fetch --prune
 git pull
 
-docker compose up -d
+docker compose up -d >> test.log
+
+
