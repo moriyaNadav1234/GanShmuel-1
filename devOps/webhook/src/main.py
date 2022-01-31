e4c5b9a1f4dde02fae4239ffec6ef58e0cb81613
dor <zdori4@gmail.com>
2022-01-31 17:22:41 +0200

    updating process.sh to catch errors and notify by mail service

--- devOps/webhook/src/main.py
+++ devOps/webhook/src/main.py
@@ -16,4 +16,4 @@ def process():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8086 ,  debug=True)
--- devOps/webhook/src/process.sh
+++ devOps/webhook/src/process.sh
@@ -12,8 +12,17 @@ function get_code() {
   #git checkout "${branch_name}" >> /app/test.log
   #git fetch --prune >> /app/test.log
   #git pull >> /app/test.log
-
+  
+  # ${?} in bash holds the last command error code. if == 0 than excuted ok. if /= 0 > error. 
+  # bash commands should run with '-e' = exit on error code. (similar to try/catch)
   docker-compose -f ./data/billing/docker-compose.yml up
+  
+ if [[ "${?}" != "0" ]]
+ then
+   echo "$(date) : ERROR! code $?" >> error.log
+   
+ fi
+ 
 }
 
 function testing() {
@@ -26,9 +35,9 @@ function testing() {
 
   if $success
   then
-    echo "Job done"
+    echo "Job done" # -> mailing service
   else
-    send_notify
+    send_notify # -> mailing service - fail msg
   fi
 }
 
@@ -40,12 +49,14 @@ function send_notify() {
   echo "Send email notify ..."
 }
 
-if [ $branch_name == 'main' ]; then
+if [ $branch_name == 'main' ]
+then
   testing
-  prod_deploy
+  prod_deploy 
 
 elif [ $branch_name == 'billing' ] || [ $branch_name == 'weight' ]; then
   testing
 
 else
   echo "Do nothing"
+fi
