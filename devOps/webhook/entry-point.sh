61906acb97328d7c5aadaa280d91653fd59ef232
Long Vu <long.vu@sunbytes.vn>
2022-02-01 15:57:47 +0700

    writing simple test for billing

--- devOps/webhook/entry-point.sh
+++ devOps/webhook/entry-point.sh
@@ -7,4 +7,6 @@ export FLASK_ENV=development
 
 flask run --host=0.0.0.0 --port=8086 > /dev/null 2>&1
 
-#TODO: if flask run fail - send mail to devOps team! 
+#TODO: if flask run fail - send mail to devOps team!
+#We don't need to do this, because this webhook service always works
+#We deploy it by manual from very first time
