38aee508dc2bc62dbda7e06db8653d3f4acd1740
dor <zdori4@gmail.com>
2022-02-02 13:51:33 +0200

    Added start.sh file for debugging (load all)

---
+++ devOps/start.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+echo "#########################"
+echo "## deleting containers ##"
+echo "#########################"
+docker container rm -f $(docker container ps -a -q)
+
+docker-compose build
+echo "q to quit, any key to continue"
+read -r -n 1 key
+if [[ $key == q ]]
+then 
+    print"\nQuitting"
+    exit
+fi
+
+docker-compose up # > dc-up.log
+echo "########################"
+echo "## entering container ##"
+echo "########################"
+
+docker exec -it ci_server bash
