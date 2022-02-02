cdcc20971e1c92877488da703aed3cbe342a6651
shZsektor <sektor_sharef@hotmail.com>
2022-02-02 10:09:00 +0200

    Added /bill with temoprary mock up data for testing. Updated Dockerfile to import requests module for py when building image

--- billing/src/api.py
+++ billing/src/api.py
@@ -5,6 +5,7 @@ import json
 import openpyxl
 from pathlib import Path
 import mysql.connector
+import requests
 
 
 class LOG_TYPE(Enum):
@@ -78,7 +79,7 @@ def Insert_provider():
 
         LOG("Inserted new record into Provider table",LOG_TYPE.INFO)
 
-        return json.dumps({'id':mycursor.lastrowid}), 200, {'ContentType':'application/json'}
+        return json.dumps({'id':mycursor.lastrowid}), 200, {'Content-Type':'application/json'}
     except ValueError:
         LOG("Failed to insert data into Provider Table",LOG_TYPE.ERROR)
     finally:
@@ -207,3 +208,134 @@ def Update_ProviderID(id):
         db.close()
     
     return "OK"
+
+
+#Mock-up routes
+@app.route('/item',methods=['GET'])
+def Get_Item():
+    res=''
+    data=None
+    with open("json-mock.json") as f:
+        data = json.load(f)
+    return json.dumps(data),200,{'Content-Type': 'application/json'}
+
+@app.route("/truck/<id>",methods=['GET'])
+def Get_Truck(id):
+    connection = connect()
+    mycursor = connection.cursor()
+
+    sql_query = "SELECT * FROM Trucks WHERE id=%s"
+    val = (id,)
+    mycursor.execute(sql_query,val)
+    results = mycursor.fetchall()
+    
+    print(f'Result: {results}')
+    LOG(f'{results}',LOG_TYPE.INFO)
+
+    #TODO Perform check to see if it's found
+    if len(results) == 0: #Empty means we don't have a truck with this license plate
+        return 404
+    
+    return requests.get('http://localhost:5000/item',{'t1':'','t2':''}).content
+
+@app.route("/weight",methods=['GET'])
+def Get_Weight():
+    data=None
+    with open("json-mock-weights.json") as f:
+        data = json.load(f)
+    return json.dumps(data),200,{'Content-Type': 'application/json'}
+
+@app.route("/bill/<id>",methods=['GET'])
+def Get_Bill(id):
+    connection = connect()
+    if connection == None:
+        return "BAD"
+    
+    name = "ad"
+    date_from = request.args.get("from")
+    date_to = request.args.get("to")
+    truckCount=0
+    total=0
+
+    LOG(f'Parameter 1: {date_from} Parameter 2: {date_to}',LOG_TYPE.INFO)
+
+    mycursor = connection.cursor()
+    
+    sql_query = "SELECT name FROM Provider WHERE id=%s"
+    val = (id,)
+    mycursor.execute(sql_query,val)
+    result_arr = mycursor.fetchall()
+    if len(result_arr) == 0:
+        return "Provider not found",404
+    
+    name = result_arr[0][0]
+
+    sql_query = "SELECT id FROM Trucks WHERE provider_id=%s"
+    val = (id,)
+    mycursor.execute(sql_query,val)
+    result_arr = mycursor.fetchall()
+    if len(result_arr) == 0:
+        return "Not trucks were registered to this provider",200
+    
+    truckCount = len(result_arr)
+
+    #We expect to get a json array for all trucks that went out
+    weighted_containers = json.loads(requests.get("http://localhost:5000/weight",{'t1':date_from,'t2':date_to,'filter':'out'}).content)
+
+    #TODO Number of sessions for each product
+    #TODO Get total weight of each product
+    providers_sessions = []
+    LOG(result_arr[0],LOG_TYPE.INFO)
+    #Get all sessions that are specific to this provider
+    for truck in weighted_containers:
+        for truck_id in result_arr[0]:
+            LOG(truck['id'],LOG_TYPE.INFO)
+            if truck['id'] == truck_id:
+                providers_sessions.append(truck)
+
+    providers_products=[]
+    
+    for truck in providers_sessions:
+        flag=0
+        for product in providers_products:
+            if truck['produce'] == product['name']:
+                product['count'] += 1
+                product['amount'] += truck['neto']
+                product['pay'] += product['rate']*truck['neto']
+                total += product['rate']*truck['neto']
+                flag=1
+        if flag == 0:#No Products were found in the list we add it
+            #Fetch the rates for this specific provider
+            sql_query = "SELECT rate FROM Rates WHERE product_id=%s AND scope=%s"
+            val = (truck['produce'],id)
+            mycursor.execute(sql_query,val)
+            rates_arr = mycursor.fetchall()
+            if len(rates_arr) == 0:#We didn't find speific rate for this provider we have to look for ALL scope
+                sql_query = "SELECT rate FROM Rates WHERE product_id=%s AND scope='ALL'"
+                val = (truck['produce'],)
+                mycursor.execute(sql_query,val)
+                rates_arr = mycursor.fetchall()
+            #TODO for now we assume product is found in rates but that's not always the case so we have to add another check
+
+            providers_products.append({
+                "name": truck['produce'],
+                "count" : 1,
+                "amount" : truck['neto'],
+                "rate": int(rates_arr[0][0]),
+                "pay" : truck['neto']*int(rates_arr[0][0])
+            })
+            total += int(rates_arr[0][0])*truck['neto']
+    
+    
+    connection.close()
+    recipet = {
+        "id": id,
+        "name": name,
+        "from": date_from,
+        "to": date_to,
+        "truckCount": truckCount,
+        "sessionCount": len(providers_sessions),
+        "products": providers_products,
+        "total": total
+    }
+    return json.dumps(recipet),200,{'Content-Type':'application/json'}
