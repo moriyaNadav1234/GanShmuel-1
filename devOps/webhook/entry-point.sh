aa2fac388af466bee9a2a56af0d44bbbcb5e6786
dor <zdori4@gmail.com>
2022-02-03 00:37:16 +0200

    flask verbos on

--- devOps/webhook/entry-point.sh
+++ devOps/webhook/entry-point.sh
@@ -5,4 +5,4 @@ pip install -r ./requirements.txt #should move to Dockerfile - RUN
 export FLASK_APP=main.py
 export FLASK_ENV=development
 
-flask run --host=0.0.0.0 --port=8086 > /dev/null 2>&1
+flask run --host=0.0.0.0 --port=8086 #> /dev/null 2>&1
