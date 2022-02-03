086df35149067f9071bcfc4fc1099be36ec4d24d
dor <zdori4@gmail.com>
2022-02-03 13:47:01 +0200

    updateing start.sh

--- devOps/start.sh
+++ devOps/start.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
 echo "#########################"
-echo "## deleting containers ##"
+echo "##  DC Down CI Server  ##"
 echo "#########################"
+echo ""
 docker-compose down
-docker container rm -f "$(docker container ps -a -q)"
-
-docker-compose build
+echo ""
 echo "#########################"
 echo "## deleting containers ##"
 echo "#########################"
+echo ""
+docker container rm -f $(docker container ps -a -q)
+echo ""
+echo "#########################"
+echo "##       DC Build      ##"
+echo "#########################"
+echo ""
+docker-compose build
 
 # echo "q to quit, any key to continue"
 # read -r -n 1 key
@@ -17,10 +24,12 @@ echo "#########################"
 #     print"\nQuitting"
 #     exit
 # fi
-
+echo ""
+echo "########################"
+echo "##       DC UP        ##"
+echo "########################"
+echo ""
 docker-compose up # > dc-up.log
-echo "########################"
-echo "## entering container ##"
-echo "########################"
+
 
 docker exec -it ci_server bash
